d955652aed86f0c00537502e8ce0b144179a1bfc
jcr0ch4 <jcr0ch4@gmail.com>
2022-10-20 14:41:31 -0300

    Update app format monetary values

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,7 @@ if uploaded_file is not None:
     st.write("Quantidade de Vendas : {}".format(df['No. Venda'].count()))
     #st.write(str( locale.getlocale() ))
     #locale.setlocale(locale.LC_MONETARY, 'pt_BR.UTF-8')
-    st.write("Valor de Venda : {}".format(df['Valor Venda'].sum())
+    st.write("Valor de Venda : {}".format(df['Valor Venda'].sum()))
     st.write("Quantidade de Vendas : {} ".format(df['No. Venda'].count()))
     valor_venda_all = df['Valor Venda'].sum()
     total_venda = df['No. Venda'].count()
